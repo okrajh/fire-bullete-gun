d3fa5185a05240048d7231ebcfcca05b6d9b99a8
Himanshu Raj <57231254+okrajh@users.noreply.github.com>
2023-03-07 15:15:43 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
 
+$('.fire-btn').click(function() {
+
+  var $gun = $(this).parent();
+
+  var $bullet = $gun.find('.bullet');
+
+  var bulletPosition = $bullet.offset();
+
+  var otherGunId = $gun.attr('id') === 'gun1' ? 'gun2' : 'gun1';
+
+  var otherGunPosition = $('#' + otherGunId).offset();
+
+  $bullet.show().animate({
+
+    top: otherGunPosition.top - bulletPosition.top,
+
+    left: otherGunPosition.left - bulletPosition.left
+
+  }, 1000, function() {
+
+    $(this).hide();
+
+  });
+
+});
+
