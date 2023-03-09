1cd809434f93125ab39c24d7dcc99757c8a9e71b
Himanshu Raj <57231254+okrajh@users.noreply.github.com>
2023-03-09 17:29:14 +0530

    Update new.php

--- new.php
+++ new.php
@@ -13,7 +13,7 @@
   position: absolute;
   top: 5px;
   left: -15px;
-  border-radius: 50%;
+adius: 50%;
 }
 
 .gun:after {
@@ -21,8 +21,46 @@
   width: 5px;
   height: 10px;
   background-color: black;
-  position: absolute;
-  top: 10px;
-  left: -5px;
-  transform: rotate(-45deg);
+
 }
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Mouseover Example</title>
+
+	<script type="text/javascript">
+
+		function onMouseOver() {
+
+			document.getElementById("myImg").style.display = "block";
+
+			document.getElementById("myPara").innerHTML = "";
+
+		}
+
+		function onMouseOut() {
+
+			document.getElementById("myImg").style.display = "none";
+
+			document.getElementById("myPara").innerHTML = "Hello, this is a paragraph.";
+
+			alert("Outside the paragraph.");
+
+		}
+
+	</script>
+
+</head>
+
+<body>
+
+	<p id="myPara" onmouseover="onMouseOver()" onmouseout="onMouseOut()">Hello, this is a paragraph.</p>
+
+	<img id="myImg" src="https://via.placeholder.com/150" style="display:none">
+
+</body>
+
+</html>
